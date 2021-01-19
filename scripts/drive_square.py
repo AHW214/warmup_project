c671dc3b81e285af29daaf8c443f6195d2740318
Adam Weider <adamweider98@gmail.com>
2021-01-19 10:08:07 -0600

    rename drive square scripts

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -1,48 +1,103 @@
 #!/usr/bin/env python3
 
-import math
-import rospy
+import lib.math as math
+import lib.vector2 as v2
 from dataclasses import dataclass
-from geometry_msgs.msg import Twist, Vector3
-from typing import List
+from lib.turtle_bot import TurtleBot, Transform
+from lib.vector2 import Vector2
+from math import pi
+from typing import List, Union
 
 
 @dataclass
-class Movement:
-    duration: float
-    velocity: Twist
+class Stop:
+    pass
 
 
-zero_vector: Vector3 = Vector3(0.0, 0.0, 0.0)
+@dataclass
+class Face:
+    target: Vector2
 
 
-def angular_velocity_z(dz: float) -> Twist:
-    return Twist(linear=zero_vector, angular=Vector3(0.0, 0.0, dz))
+@dataclass
+class Approach:
+    target: Vector2
+    initial_distance: float
 
 
-def linear_velocity_x(dx: float) -> Twist:
-    return Twist(linear=Vector3(dx, 0, 0), angular=zero_vector)
+State = Union[Stop, Face, Approach]
 
 
-def send_movements(publisher: rospy.Publisher, moves: List[Movement]) -> None:
-    for move in moves:
-        publisher.publish(move.velocity)
-        rospy.sleep(move.duration)
+class DriveSquare(TurtleBot):
+    to_visit: Vector2 = [Vector2(3, 0), Vector2(3, 3), Vector2(0, 3), Vector2(0, 0)]
+    visited: List[Vector2] = []
+    state: State = Stop()
+
+    def update(self, transform: Transform, deltaTime: float) -> None:
+        if isinstance(self.state, Stop):
+            if not self.to_visit:
+                self.to_visit.extend(self.visited)
+                self.visited = []
+
+            target, *rest = self.to_visit
+            self.state = Face(target)
+            self.to_visit = rest
+
+        elif isinstance(self.state, Face):
+            d_theta = delta_angle(
+                transform.rotation, transform.position, self.state.target
+            )
+
+            if abs(d_theta) < 1e-2:
+                target = self.state.target
+                distance_to_target = v2.distance_between(transform.position, target)
+
+                self.state = Approach(
+                    target=target, initial_distance=distance_to_target
+                )
+            else:
+                sn = -1 * math.sign(d_theta)
+                self.send(velocity_angular=sn * 0.5, velocity_linear=0.0)
+
+        elif isinstance(self.state, Approach):
+            target = self.state.target
+            initial_distance = self.state.initial_distance
+
+            if approx_equal(transform.position, target):
+                self.send(velocity_angular=0.0, velocity_linear=0.0)
+                self.visited.append(target)
+                self.state = Stop()
+            else:
+                half_init_dist = initial_distance / 2
+                current_distance = v2.distance_between(transform.position, target)
+                midpoint_dist = abs(half_init_dist - current_distance)
+                interpolation = 1 - (midpoint_dist / half_init_dist)
+
+                vel_linear = math.smoothstep(0.2, 1, interpolation)
+
+                d_theta = delta_angle(transform.rotation, transform.position, target)
+
+                sn = -1 * math.sign(d_theta)
+                angle_thing = abs(d_theta)
+                inter = (0 if angle_thing < 1e-2 else angle_thing) / pi
+                vel_angular = (
+                    sn
+                    * max(15 * current_distance, 1)
+                    * math.smoothstep(0.0, 2 * pi, inter)
+                )
+
+                self.send(velocity_angular=vel_angular, velocity_linear=vel_linear)
 
 
-def drive_square(publisher: rospy.Publisher) -> None:
-    drive_line = Movement(duration=10, velocity=linear_velocity_x(0.2))
-    turn_corner = Movement(duration=4, velocity=angular_velocity_z(math.pi / 8))
-
-    send_movements(publisher, [drive_line, turn_corner])
+def approx_equal(v: Vector2, w: Vector2) -> bool:
+    return v2.sqr_magnitude(v2.sub(v, w)) < 1e-3
 
 
-def run() -> None:
-    rospy.init_node("drive_square")
-    publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-
-    while True:
-        drive_square(publisher)
+def delta_angle(rotation: float, source: Vector2, target: Vector2) -> float:
+    direction_target = v2.normalize(v2.sub(target, source))
+    direction_facing = v2.from_angle(rotation)
+    return v2.signed_angle_between(direction_target, direction_facing)
 
 
-run()
+if __name__ == "__main__":
+    DriveSquare().run()

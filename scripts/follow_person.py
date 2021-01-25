49e0cd7a40664e8b2a9731b1df161275d706c106
Adam Weider <adamweider98@gmail.com>
2021-01-25 07:56:28 -0600

    track closest object

--- scripts/follow_person.py
+++ scripts/follow_person.py
@@ -5,23 +5,23 @@ Have TurtleBot follow nearby objects.
 """
 
 from dataclasses import dataclass
+from enum import Enum
 import math
 from typing import List, Optional, Tuple
 import rospy
 from sensor_msgs.msg import LaserScan
 from lib.controller import Cmd, Controller, Sub
+import lib.mathf as mathf
 import lib.turtle_bot as tb
 
 
 ### Model ###
+class Model(Enum):
+    stop = 1
+    follow = 2
 
 
-@dataclass
-class Model:
-    state: str
-
-
-init_model: Model = Model(state="")
+init_model: Model = Model.stop
 
 
 ### Events ###
@@ -35,7 +35,10 @@ class Scan:
 
 def closest_in_scan(scan: LaserScan) -> Scan:
     (count, distance) = min(enumerate(scan.ranges), key=lambda t: t[1])
-    direction = scan.angle_increment * count
+
+    angle = scan.angle_increment * count
+    direction = angle - (2 * math.pi if angle > math.pi else 0.0)
+
     return Scan(distance, direction)
 
 
@@ -43,7 +46,17 @@ def closest_in_scan(scan: LaserScan) -> Scan:
 
 
 def update(scan: Scan, model: Model) -> Tuple[Model, Optional[Cmd]]:
-    print(scan)
+    if model == Model.follow:
+        interpolation = abs(scan.direction) / math.pi
+        direction = mathf.sign(scan.direction)
+        vel_angular = direction * mathf.smoothstep(0.0, 2 * math.pi, interpolation)
+
+        return (model, tb.velocity(linear=0.0, angular=vel_angular))
+
+    # if isinstance(model, Stop):
+    if scan.distance > 0.5:
+        return (Model.follow, None)
+
     return (model, None)
 
 
